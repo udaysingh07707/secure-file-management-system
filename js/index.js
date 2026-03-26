b77c87da4f3f1e30c5002f43113da92f2f224a03
Rishav verma <Rishavverma12.100@gmail.com>
2026-03-27 00:56:08 +0530

    Added login page

---
+++ js/index.js
@@ -0,0 +1,7 @@
+document.getElementById("loginForm").addEventListener("submit", function(e) {
+  e.preventDefault();
+
+  alert("Login button clicked 🚀");
+
+  // later you will connect API here
+});
